2e861f2d3906a934f9f071117d27b816d76d24d9
Gementos <146087651+Gementos@users.noreply.github.com>
2023-10-13 12:22:09 +0500

    Create distant.py

---
+++ distant.py
@@ -0,0 +1,35 @@
+#1
+
+def multiply(a,b):
+  i = a * b
+  return i
+multiply(2,3)
+
+#2
+
+def
+make_negative( number ):
+  return -abs(nember)
+
+#3
+
+def positive_sum(arr):
+  return sum(i for i in arr if i > 0)
+
+#4
+
+def solution(string):
+  i = ".join(reversed(string))
+  return i
+
+solution('hello')
+
+#5
+
+def evenOdd(number):
+  if number % 2 == 0:
+    return 'even'
+  else:
+    return 'odd'
+
+print(evenOdd(3))
